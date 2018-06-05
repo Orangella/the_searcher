b7b073fa1b968dc45de8478e3f590b94858a2037
Orangella <korin_nyashka@mail.ru>
2018-06-05 22:51:57 +0700

    Add 'unique' and 'count' logic

--- searcher.py
+++ searcher.py
@@ -10,17 +10,26 @@ def create_parser():
         ['-u', 'unique', 'store_true'],
         ['-c', 'count', 'store_true'],
         ['-l', 'count_lines', 'store_true'],
-        ['-s', 'sort', 'store_const'],
-        ['-o', 'order', 'store_const'],
-        ['-n', 'first_n', 'store_const'],
-        ['-stat', 'statistic', 'store_const']
+        ['-s', 'sort', 'store'],
+        ['-o', 'order', 'store'],
+        ['-n', 'first_n', 'store'],
+        ['-stat', 'statistic', 'store']
     ]
     for option, name, action in options:
-        parser.add_argument(option, dest= name, action=action, nargs='?')
+        parser.add_argument(option, dest=name, action=action)
+
     parser.add_argument('reg', nargs=1)
     parser.add_argument('file', nargs=1)
 
     return parser
+
+
+def get_matches_list(reg, file):
+    results = []
+    for line in file:
+        result = re.search(reg, line).group(0)
+        results.append(result)
+    return results
 
 
 if __name__ == '__main__':
@@ -30,6 +39,13 @@ if __name__ == '__main__':
     print(params)
     if params.file:
         with open(params.file[0], 'r') as file:
-            for line in file:
-                result = re.search(params.reg[0], line).group(0)
-                print(result)
+            if params.unique:
+                results = get_matches_list(params.reg[0], file)
+                results = set(results)
+                if params.count:
+                    print(len(results))
+                else:
+                    print(results)
+            elif params.count:
+                results = get_matches_list(params.reg[0], file)
+                print(len(results))
