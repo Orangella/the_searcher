47494731a03b038591aa1a78a33c8162f55cc28d
Orangella <korin_nyashka@mail.ru>
2018-06-09 22:22:09 +0700

    Create 'sort' logic

--- searcher.py
+++ searcher.py
@@ -26,23 +26,69 @@ def create_parser():
 def get_matches_list(reg, file):
     results = []
     for line in file:
-        try:
-            result = re.search(reg, line).group(0)
-        except:
-            continue
-        results.append(result)
+        result = re.findall(reg, line)
+        results = results + result
     return results
+
+
+def count_matches(reg, file):
+    counter = 0
+    for line in file:
+        result = re.findall(reg, line)
+        counter += len(result)
+    return counter
 
 
 def count_lines(reg, file):
     counter = 0
     for line in file:
-        try:
-            result = re.search(reg, line).group(0)
-        except:
-            continue
-        counter += 1
+        result = re.search(reg, line)
+        if(result):
+            counter += 1
     return counter
+
+
+def get_statistic_dict(reg, file):
+    results = {}
+    for line in file:
+        result = re.findall(reg, line)
+        for item in result:
+            if item in results:
+                results[item] += 1
+            else:
+                results[item] = 1
+    return results
+
+
+def sort_matches(reg, file, freq_or_abc):
+    statistic_dict = get_statistic_dict(reg, file)
+    sorted_results = []
+    if freq_or_abc == 'freq':
+        sorted_results = sorted(statistic_dict.items(), key=lambda x: x[1])
+        sorted_results = [x[0] for x in sorted_results]
+    if freq_or_abc == 'abc':
+        sorted_results = sorted(statistic_dict.keys())
+    return sorted_results
+
+
+def stat(reg, file, freq_or_abc):
+    results = {}
+    sorted_results = []
+    for line in file:
+        result = re.findall(reg, line)
+        for item in result:
+            if item in results:
+                results[item] += 1
+            else:
+                results[item] = 1
+    if freq_or_abc == 'freq':
+        s = sorted(results.items(), key=lambda x: x[1])
+        for k in sorted(results.items(), key=lambda x: x[1]):
+            sorted_results.append([k[0], results[k[0]]])
+    if freq_or_abc == 'abc':
+        for k in sorted(results.keys()):
+            sorted_results.append([k, results[k]])
+    return sorted_results
 
 
 if __name__ == '__main__':
@@ -60,7 +106,11 @@ if __name__ == '__main__':
                 else:
                     print(results)
             elif params.count:
-                results = get_matches_list(params.reg[0], file)
-                print(len(results))
+                print(count_matches(params.reg[0], file))
             elif params.count_lines:
                 print(count_lines(params.reg[0], file))
+            elif params.sort:
+                if params.sort == 'freq':
+                    print(sort_matches(params.reg[0], file, 'freq'))
+                elif params.sort == 'abc':
+                    print(sort_matches(params.reg[0], file, 'abc'))
--- tests.py
+++ tests.py
@@ -4,14 +4,27 @@ import searcher
 
 class SearcherTests(unittest.TestCase):
 
-  def test_get_matches_list(self):
-      self.assertEqual(searcher.get_matches_list('a', ['aa']), ['a'])
-      self.assertEqual(searcher.get_matches_list('a', ['aa','a']), ['a', 'a'])
+    def test_get_matches_list(self):
+      self.assertEqual(searcher.get_matches_list('a', [' aa']), ['a', 'a'])
+      self.assertEqual(searcher.get_matches_list('a', ['aba','a']), ['a', 'a', 'a'])
 
-  def test_count_lines(self):
-      self.assertEqual(searcher.count_lines('a', ['aa']), 1)
-      self.assertEqual(searcher.count_lines('a', ['aa', 'a']), 2)
+    def test_count_matches(self):
+      self.assertEqual(searcher.count_matches('a', ['aa']), 2)
+      self.assertEqual(searcher.count_matches('a', ['aa', 'a']), 3)
 
+    def test_count_lines(self):
+        self.assertEqual(searcher.count_lines('a', ['aa']), 1)
+        self.assertEqual(searcher.count_lines('a', ['aa', 'a']), 2)
+
+    def test_sort_matches(self):
+        self.assertEqual(searcher.sort_matches('\w+@([\w.-_]+)',
+                                               ['test@ytr.com test@mail.ru '
+                                                'test@mail.ru'], 'abc'),
+                         ['mail.ru', 'ytr.com'])
+        self.assertEqual(searcher.sort_matches('\w+@([\w.-_]+)',
+                                               ['test@ytr.com test@mail.ru '
+                                                'test@mail.ru'], 'freq'),
+                         ['ytr.com', 'mail.ru'])
 
 if __name__ == '__main__':
     unittest.main()
